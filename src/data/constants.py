499b9f0e54c4a5068cdd64dd094a56046ed42bb5
Federica Latora <72083378+FedericaLatora@users.noreply.github.com>
2022-06-11 16:02:03 +0200

    Added constants

---
+++ src/data/constants.py
@@ -0,0 +1,68 @@
+###############################################
+#        CONSTANTS FOR REDUCED DATASET        #
+###############################################
+
+# Sampling rate
+SAMPLING_RATE = 16000
+# Number of categories
+num_categories_reduced = 10
+
+# Encoding of the 10-classes 
+labels_10_classes = {
+'down': 0, 
+'go': 1,
+'left': 2,
+'no': 3, 
+'off': 4, 
+'on': 5,
+'right': 6,
+'stop': 7, 
+'up': 8, 
+'yes': 9
+}
+
+###############################################
+#       CONSTANTS FOR COMPLETE DATASET        #
+###############################################
+
+# Number of categories for the complete dataset
+num_categories_complete = 35
+
+# Encoding of the 35-classes 
+labels_35_classes = {
+"backward":0,
+"bed":1,
+"bird":2,
+"cat":3,
+"dog":4,
+"down":5,
+"eight":6,
+"five":7,
+"follow":8,
+"forward":9,
+"four":10,
+"go":11,
+"happy":12,
+"house":13,
+"learn":14,
+"left":15,
+"marvin":16,
+"nine":17,
+"no":18,
+"off":19,
+"on":20,
+"one":21,
+"right":22,
+"seven":23,
+"sheila":24,
+"six":25,
+"stop":26,
+"three":27,
+"tree":28,
+"two":29,
+"up":30,
+"visual":31,
+"wow":32,
+"yes":33,
+"zero":34
+}
